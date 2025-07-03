5be916de11f24093cca3e45dbda165620ddf6c0f
dabin <binda0624@gmail.com>
2025-07-03 14:44:21 +0900

    feat: 홈 헤더 UI 구조 및 스타일링 초기 구현

--- src/features/Main/Home.jsx
+++ src/features/Main/Home.jsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import React,{useState} from 'react';
+import HomeHeader from '../../components/Home/HomeHeader';
 
 function Home() {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [selectedCategory, setSelectedCategory] = useState('');
   
+  return (
+    <div>
+      <HomeHeader/>
+    </div>
+  );
 }
 
 export default Home;
